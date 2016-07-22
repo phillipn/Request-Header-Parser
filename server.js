18ec1020a864b71448907e69e033f74d86b17639
Nick Phillips <phillipn101@gmail.com>
2016-07-22 11:45:59 -0500

    Edited IP display

--- server.js
+++ server.js
@@ -4,12 +4,14 @@ var http = require('http');
 var net = require('net');
 
 
-app.get('/api/whoami', function(req, res){
+app.get('/', function(req, res){
   var UAParser = require('ua-parser-js');
   var parser = new UAParser();
   var os = parser.setUA(req.headers['user-agent']).getOS();
   var lang = req.headers['accept-language'].split(',')[0];
-  var ip = req.connection.remoteAddress;
+  var requestIp = req.connection.remoteAddress;
+  var indexOfColon = requestIp.lastIndexOf(':');
+  var ip = requestIp.slice(indexOfColon+1);
   res.end(JSON.stringify({OS: os.name + ' ' + os.version, Language: lang, IPAddress: ip}));
 })
 
